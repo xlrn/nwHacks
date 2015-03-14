d7c66dffea479838859e6b9669e6e3c609f4cea3
jshe <jennifershe123@gmail.com>
2015-03-14 11:34:28 -0700

    changed layout of tasks, added taskManager

--- app/src/main/java/com/example/nelson/carbongame/model/Bike.java
+++
@@ -1,8 +0,0 @@
-package com.example.nelson.carbongame.model;
-
-/**
- * Created by Jennifer on 2015-03-13.
- */
-public class Bike extends Transit {
-    public static int rate = 5;
-}
--- app/src/main/java/com/example/nelson/carbongame/model/TimeBased.java
+++ app/src/main/java/com/example/nelson/carbongame/model/TimeBased.java
@@ -6,11 +6,12 @@ import com.example.nelson.carbongame.UI.Timer;
 /**
  * Created by Jennifer on 2015-03-13.
  */
-public abstract class TimeBased {
+public abstract class TimeBased implements Task {
     private static int rate;
     private static int init;
     private static Timer timer;
     private boolean isOn;
+    private String tag;
 
     public TimeBased(int rate, int init) {
         this.rate = rate;
